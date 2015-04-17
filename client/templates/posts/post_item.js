bf4f5992027a2d91cc7c21ab8d4928871fcd7d8d
rondakarto <rondakarto@gmail.com>
2015-04-17 18:23:40 +0900

    아이언 라우터

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -2,7 +2,9 @@ Template.postItem.helpers({
     domain: function() {
         var a = document.createElement('a');
         a.href = this.url;
-        console.log( this.url );
+        console.log( 'this:url : ' + this.url );
+        console.log( a.hostname );
         return a.hostname;
+
     }
 });
