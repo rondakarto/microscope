07d25ad6c9c5205c8e35ff82b79fe075c9f66dc4
rondakarto <rondakarto@gmail.com>
2015-04-27 18:56:05 +0900

    등록

---
+++ client/templates/posts/post_submit.js
@@ -0,0 +1,14 @@
+Template.postSubmit.events( {
+    'submit form': function(e) {
+        e.preventDefault();
+
+        var post = {
+            url: $(e.target).find('[name=url]').val(),
+            title: $(e.target).find('[name=title]').val()
+        };
+
+        post._id = Posts.insert(post);
+        console.log( 'url: ' + post.url );
+        Router.go('postPage',post);
+    }
+});
